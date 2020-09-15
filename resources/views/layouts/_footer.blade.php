5756b443109a7afcae0dd9a2116d4a0740025d6f
vagrant <jack.xu.nz@gmail.com>
2020-09-15 10:51:34 +0000

    blade into parts; routes naming

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+
+    <div class="float-right">
+        <a href="{{route('about')}}">About</a>
+    </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,18 +7,11 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">Delishows</a>
-            <ul class="navbar-nav justify-content-end">
-
-                <li class="nav-item"><a class="nav-link" href="#">Sign In</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Sign Up</a></li>
-                <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-            </ul>
-        </div>
-    </nav>
-    <div class="container">@yield('content')</div>
+    @include('layouts._header')
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 
 </body>
 
