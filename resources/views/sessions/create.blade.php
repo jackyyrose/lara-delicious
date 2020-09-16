9520802d3c32198b987dec6246d65cd4e5c60600
vagrant <jack.xu.nz@gmail.com>
2020-09-16 11:18:47 +0000

    session auth when signing in

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.default')
+@section('title','Sign In')
+
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+    <div class="card">
+        <div class="card-header">
+            <h5>Sign In</h5>
+        </div>
+        <div class="card-body">
+            @include('shared._errors')
+            <form method="POST" action="{{route('login')}}">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email：</label>
+                    <input type="text" name="email" class="form-control" value="{{ old('email')}}">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password：</label>
+                    <input type="password" name="password" class="form-control" value="{{ old('password')}}">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Sign In</button>
+            </form>
+            <hr>
+            <p>Don't have an account yet? <a href="{{ route('signup') }}">Sign Up Now!</a></p>
+        </div>
+    </div>
+</div>
+
+@endsection
