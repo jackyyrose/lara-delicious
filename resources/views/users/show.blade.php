f453c153a4a011ea94dc863282081b36a267ef61
Jack <jack.xu.nz@gmail.com>
2020-09-28 23:51:52 +1300

    paginator use bootstrap over tailwind

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,23 +2,29 @@
 @section('title',$user->name)
 @section('content')
 <div class="row">
-    <section class="user_info">
-        @include('shared._user_info', ['user'=>$user])
-    </section>
-    <section class="status">
-        @if ($reviews->count() > 0)
-        <ul class="list-unstyled">
-            @foreach ($reviews as $review)
-                @include('reviews._review')
-            @endforeach
+    <div class="offset-md-2 col-md-8">
+        <div class="col-md-12">
+            <div class="offset-md-2 col-md-8">
+                <section class="user_info">
+                    @include('shared._user_info', ['user'=>$user])
+                </section>
+                <section class="status">
+                    @if ($reviews->count() > 0)
+                    <ul class="list-unstyled">
+                        @foreach ($reviews as $review)
+                        @include('reviews._review')
+                        @endforeach
 
-        </ul>
-        <div class="mt-5">
-            {{ $reviews->links() }}
+                    </ul>
+                    <div class="mt-5">
+                        {{ $reviews->links() }}
+                    </div>
+                    @else
+                    <p>No reviews！</p>
+                    @endif
+                </section>
+            </div>
         </div>
-        @else
-        <p>No reviews！</p>
-        @endif
-    </section>
+    </div>
 </div>
 @stop
