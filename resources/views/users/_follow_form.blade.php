c5ac89bdf9ad76ef6020d184cd36849203b2f080
vagrant <jack.xu.nz@gmail.com>
2020-10-05 08:51:57 +0000

    to follow & to unfollow; set policy for follower controller

---
+++ resources/views/users/_follow_form.blade.php
@@ -0,0 +1,20 @@
+@can('follow', $user)
+<div class="text-center mt-2 mb-4">
+    @if(Auth::user()->isFollowing($user->id))
+
+    <form action="{{route('followers.destroy', $user->id)}}" method="post">
+        @csrf
+        {{method_field('DELETE')}}
+        <button type="submit" class="btn btn-sm btn-outline-primary">Cancel Follow</button>
+    </form>
+    @else
+    <form action="{{route('followers.store', $user->id)}}" method="post">
+        @csrf
+        <button type="submit" class="btn btn-sm btn-primary"> Follow</button>
+    </form>
+
+
+    @endif
+</div>
+
+@endcan
